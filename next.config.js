97ceb8f5105a09b70ab471546489b3f56d31eae2
rahmattSera <Rahmat.Hidayatullah@sera.astra.co.id>
2024-01-10 13:51:38 +0700

    connect to mongodb, and add models rooms

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    DB_LOCAL_URI:
+      "mongodb+srv://rahmathidayatullah996:4fBHgJMmvrvPxlxB@bookitv2database.vqrwmxe.mongodb.net/?retryWrites=true&w=majority",
+    DB_URI:
+      "mongodb+srv://rahmathidayatullah996:4fBHgJMmvrvPxlxB@bookitv2database.vqrwmxe.mongodb.net/?retryWrites=true&w=majority",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
