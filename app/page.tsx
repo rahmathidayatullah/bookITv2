e2f238e3853107549f776e45aba220d93c51f376
rahmattSera <Rahmat.Hidayatullah@sera.astra.co.id>
2024-01-11 13:47:56 +0700

    handle error, list room, detail room and more component room

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
 import Home from "@/components/Home";
+import Error from "./error";
 
-export default function HomePage() {
-  return <Home />;
+export const metadata = {
+  title: "HomePage - BookIT",
+};
+
+const getRooms = async () => {
+  const res = await fetch(`${process.env.API_URL}/api/rooms`);
+  return res.json();
+};
+
+export default async function HomePage() {
+  const data = await getRooms();
+
+  if (data?.message) {
+    return <Error error={data} />;
+  }
+
+  return <Home data={data} />;
 }
--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    API_URL: "http://localhost:3000",
     DB_LOCAL_URI:
       "mongodb+srv://rahmathidayatullah996:4fBHgJMmvrvPxlxB@bookitv2database.vqrwmxe.mongodb.net/?retryWrites=true&w=majority",
     DB_URI:
       "mongodb+srv://rahmathidayatullah996:4fBHgJMmvrvPxlxB@bookitv2database.vqrwmxe.mongodb.net/?retryWrites=true&w=majority",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
